4e4153a88b317accab6124ab36857e5d2e3fe372
ergentus <ponfable999@gmail.com>
2023-03-13 22:04:40 +0300

    input add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
+import { v1 } from 'uuid';
 import './App.css';
 import { TaskType, TodoList } from './TodoList';
 
 export type FilterValuesType = 'all' | 'active' | 'completed'
 
 function App(): JSX.Element {
+	console.log(v1());
 
 	const [tasks, setTasks] = useState<TaskType[]>([
-		{ id: 1, title: "HTML & CSS", isDone: true },
-		{ id: 2, title: "CSS & SCSS", isDone: true },
-		{ id: 3, title: "ES6/TS", isDone: false },
-		{ id: 4, title: "REDUX", isDone: false }
+		{ id: v1(), title: "HTML & CSS", isDone: true },
+		{ id: v1(), title: "CSS & SCSS", isDone: true },
+		{ id: v1(), title: "ES6/TS", isDone: false },
+		{ id: v1(), title: "REDUX", isDone: false }
 	])
 
 	const [filter, setFilter] = useState<FilterValuesType>('all')
@@ -23,13 +25,27 @@ function App(): JSX.Element {
 		setFilter(name)
 	}
 
-	const removeTask = (taskId: number) => {
+	const removeTask = (taskId: string) => {
 		setTasks(tasks.filter((task: TaskType) => task.id !== taskId))
+	}
+
+	const addTask = (title: string) => {
+		const newTask: TaskType = {
+			id: v1(),
+			title: title,
+			isDone: false
+		}
+		setTasks([newTask, ...tasks])
 	}
 
 	return (
 		<div className="App">
-			<TodoList title={'What to learn'} tasks={tasksForRender} removeTask={removeTask} listFilter={listFilter} />
+			<TodoList
+				title={'What to learn'}
+				tasks={tasksForRender}
+				removeTask={removeTask}
+				addTask={addTask}
+				listFilter={listFilter} />
 		</div>
 	);
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 import { FilterValuesType } from './App';
 
 export type TaskType = {
-   id: number
+   id: string
    title: string
    isDone: boolean
 }
@@ -10,8 +10,9 @@ export type TaskType = {
 type TodoListProbsType = {
    title: string
    tasks: TaskType[]
-   removeTask: (taskId: number) => void
+   removeTask: (taskId: string) => void
    listFilter: (name: FilterValuesType) => void
+   addTask: (title: string) => void
 }
 
 export const TodoList = (props: TodoListProbsType): JSX.Element => {
@@ -25,21 +26,54 @@ export const TodoList = (props: TodoListProbsType): JSX.Element => {
    }
 
    const todoListItem: JSX.Element[] = props.tasks.map((task: TaskType) => {
+
+      const removeTaskHandler = () => props.removeTask(task.id)
+
       return (
-         <li>
+         <li key={task.id}>
             <input type="checkbox" checked={task.isDone} />
             <span>{task.title}</span>
-            <button onClick={() => { props.removeTask(task.id) }}>x</button>
+            <button onClick={removeTaskHandler}>x</button>
          </li>
       )
    })
+
+   const [title, setTitle] = useState<string>('')
+
+   const maxTitleLength = 20
+   const recommendedTitleLength = 10
+   const isAddTaskPossible: boolean = title.length === 0 || title.length > maxTitleLength
+
+   const addTaskHandler = () => {
+      props.addTask(title)
+      setTitle('')
+   }
+   const setLocalTitleHandler = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
+   const onEnterDownTaskHandler = isAddTaskPossible
+      ? undefined
+      : (e: KeyboardEvent<HTMLInputElement>) => e.key === 'Enter' && addTaskHandler()
+
+   const longTitleWarningMessage = (title.length > recommendedTitleLength && title.length <= maxTitleLength) &&
+      <div style={{ color: 'yellow' }}>Title should be shorter</div>
+   const longTitleErrorMessage = title.length > maxTitleLength &&
+      <div style={{ color: 'red' }}>Title should is too long</div>
 
    return (
       <div className={isAllTasksNotIsDone} >
          <h3>{props.title}</h3>
          <div>
-            <input />
-            <button>+</button>
+            <input
+               placeholder='Enter task title, please'
+               value={title}
+               onChange={setLocalTitleHandler}
+               onKeyDown={onEnterDownTaskHandler}
+            />
+            <button
+               onClick={addTaskHandler}
+               disabled={isAddTaskPossible}>+
+            </button>
+            {longTitleWarningMessage}
+            {longTitleErrorMessage}
          </div>
          <ul>
             {todoListItem}
